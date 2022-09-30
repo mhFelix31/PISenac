49879eeac703aa37c5164c17ae0e66b5fd59b6a6
mhFelix31 <113628306+mhFelix31@users.noreply.github.com>
2022-09-30 16:27:01 -0300

    Project files to import
    This commit is a change in the importation method applied

---
+++ LanchoneteImport/src/lanchonete/Lanchonete.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lanchonete;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.sql.*;
+/**
+ *
+ * @author mateus.felix
+ */
+public class Lanchonete extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+      
+    private SqlHandler sql = new SqlHandler();
+    
+    
+    
+    public void Inicialzação(){
+        Connection conn = sql.Conect("localhost","3306","lanchonete");
+        Statement st = sql.state(conn);
+        
+        
+        sql.close(conn);
+    }
+    
+    
+    
+    
+}
---
+++ LanchoneteImport/src/lanchonete/Produto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lanchonete;
+
+/**
+ *
+ * @author mateus.felix
+ */
+public class Produto {
+    private int id;
+    private String nome;
+    private float preco;
+    private String descricao;
+    private String categoria;
+    private String tipoDeCobranca;
+    private String materiaPrima;
+    private float validade;
+    private float fabricacao;
+    
+    
+    
+    
+    
+    
+}
